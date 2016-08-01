089fb2044bcb342d64d8ce3d2f1a809af42a7bd5
Karina Denisevich <karina_41997@mail.ru>
2016-08-01 17:13:19 +0300

    master: main added and 2 files changed

--- src/main/java/com/github/Karina_Denisevich/spring/hello_world/core/App.java
+++ src/main/java/com/github/Karina_Denisevich/spring/hello_world/core/App.java
@@ -1,4 +1,16 @@
 package com.github.Karina_Denisevich.spring.hello_world.core;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext(
+                "conf.xml");
+
+        HelloWorld obj = (HelloWorld) context.getBean("helloBean");
+        obj.printHello();
+    }
 }
--- src/main/java/com/github/Karina_Denisevich/spring/hello_world/core/HelloWorld.java
+++ src/main/java/com/github/Karina_Denisevich/spring/hello_world/core/HelloWorld.java
@@ -3,12 +3,17 @@ package com.github.Karina_Denisevich.spring.hello_world.core;
 public class HelloWorld {
 
     private String value;
+    private String value1;
 
     public void setValue(String value) {
         this.value = value;
     }
 
+    public void setValue1(String name){
+        this.value1 = name;
+    }
+
     public void printHello() {
-        System.out.println(value);
+        System.out.println(value + value1);
     }
 }
