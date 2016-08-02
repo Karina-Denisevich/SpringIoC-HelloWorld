6b74af8d2785b9d137d818912e9c80a8ea49ffcc
Karina Denisevich <karina_41997@mail.ru>
2016-08-02 13:59:44 +0300

    master: class Person added

---
+++ src/main/java/com/github/Karina_Denisevich/spring/hello_world/core/Person.java
@@ -0,0 +1,20 @@
+package com.github.Karina_Denisevich.spring.hello_world.core;
+
+public class Person {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person [name=" + name + "]";
+    }
+
+}
