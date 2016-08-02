02a2e317c3109648182964dea01761dda32995e1
Karina Denisevich <karina_41997@mail.ru>
2016-08-02 15:06:44 +0300

    master: Autowired added

--- src/main/java/com/github/Karina_Denisevich/spring/hello_world/core/HelloWorld.java
+++ src/main/java/com/github/Karina_Denisevich/spring/hello_world/core/HelloWorld.java
@@ -1,7 +1,11 @@
 package com.github.Karina_Denisevich.spring.hello_world.core;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class HelloWorld {
 
+    @Autowired(required=false)
+    private Person person;
     private String value;
     private String value1;
 
@@ -13,7 +17,22 @@ public class HelloWorld {
         this.value1 = name;
     }
 
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorld{" +
+                "person=" + person +
+                ", value='" + value + '\'' +
+                ", value1='" + value1 + '\'' +
+                '}';
+    }
+
     public void printHello() {
         System.out.println(value + value1);
     }
+
+
 }
